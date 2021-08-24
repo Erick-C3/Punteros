c7155526a4107146648cd5b5ee7502935e939039
Erick-C3 <81482790+Erick-C3@users.noreply.github.com>
2021-08-24 12:55:35 -0300

    Add files via upload

---
+++ pasajeReferencia.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+const int DNI_NULO = -1;
+
+/*
+	Descripcion: cambia el numero del dni
+*/
+void cambiarDni(int *dniPersona){//
+	printf("Dni anterior: %i\n", *dniPersona);
+	printf("Ingrese su dni\n");
+	scanf("%i", dniPersona);//dniPersona tiene la direccion de memoria de dni
+							// es como hacer esto: &dni
+}
+
+
+
+
+int main(){
+	
+	int dni = DNI_NULO;
+	cambiarDni(&dni);//pasar la direccion de memoria de la variable dni 0X23BF52
+	printf("Dni ingresado: %i\n", dni );
+
+	return 0;
+}
