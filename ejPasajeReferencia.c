8dc1047180f0b7dd05864fdceabdd1fbefcf4c52
Erick-C3 <81482790+Erick-C3@users.noreply.github.com>
2021-08-31 09:14:26 -0300

    Add files via upload

---
+++ ejPasajeReferencia.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+const int SIN_OPCION = -1;
+#define CAMBIAR_NUM 1
+#define NO_CAMBIAR_NUM 0
+
+/*
+	Descripcion: da la opcion de cambiar el numero de contacto
+*/
+int opcCambiarNumContacto(int *puntNumContacto, char *puntInicialUsuario){
+	int opcionUsuario = SIN_OPCION;
+
+	// * operador de indireccion
+	// & operador de direccion
+	printf("Numero de contacto actual: %i\n", *puntNumContacto );// mostramos el contenido
+	printf("Desea cambiar numero de contacto? 1-Si 0-No\n");
+	scanf("%i", &opcionUsuario);// opcionUsuario con el & que nos de su direccion de memoria
+
+	switch(opcionUsuario){
+		case CAMBIAR_NUM:
+			printf("Cambiando el numero de contacto\n");
+			printf("Ingrese numero numero de contacto:\n");
+			scanf("%i", puntNumContacto);// guarda el nuevo valor en la direccion de  memoria contenida
+			printf("Tambien necesitas cambiar la inicial de usuario\n");
+			scanf(" %c", puntInicialUsuario);
+			break;
+		case NO_CAMBIAR_NUM:
+			printf("Numero de contacto no alterado\n");
+	}
+
+	return opcionUsuario;
+}
+
+
+int main(){
+
+	int numContacto = 1100000000;
+	char inicialUsuario = 'E';
+
+	if( opcCambiarNumContacto(&numContacto, &inicialUsuario) ){//envia la direcc de memoria de numContacto
+		printf("El numero de contacto fue alterado por: %i\n", numContacto );
+		printf("Nueva inicial del usuario: %c\n", inicialUsuario );
+	}else{
+		printf("Numero sigue sin alterarse: %i\n", numContacto);
+	}
+
+	return 0;
+}
